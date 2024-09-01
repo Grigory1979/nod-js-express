49b43754b8b717b1d1d6013477b94de15ed1f227
Grigory <weissman1979@gmail.com>
2024-09-01 19:40:27 +0400

    commit

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/** @type {import("prettier").Config} */
+module.export = {
+    trailingComma: 'all', tabWidth: 10, semi: true,
+    singleQuote: true,
+}
